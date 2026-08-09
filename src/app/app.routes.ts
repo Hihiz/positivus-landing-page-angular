c17910a91b10b9bfe8aeb0ee6bcb76958704fd7c
Hihiz <public@mail.ru>
2026-08-09 23:15:50 +0300

    Добавить роуты

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadComponent: () => import('./features/home/page/home-page.component').then((c) => c.HomePageComponent),
-  // },
-  // {
-  //   path: 'blog',
-  //   loadComponent: () => import('./features/blog/page/blog-page.component').then((c) => c.BlogPageComponent),
-  // },
-  // {
-  //   path: '**',
-  //   redirectTo: '',
-  // },
+  {
+    path: '',
+    loadComponent: () => import('./features/home/page/home-page.component').then((c) => c.HomePageComponent),
+  },
+  {
+    path: 'blog',
+    loadComponent: () => import('./features/blog/page/blog-page.component').then((c) => c.BlogPageComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
