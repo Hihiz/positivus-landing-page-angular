930a206e3bf2b00ec794ae4b42a6449f9401e434
Hihiz <public@mail.ru>
2026-08-09 23:10:46 +0300

    Добавить модели главной страницы

---
+++ src/app/features/home/models/team-item.ts
@@ -0,0 +1,9 @@
+export interface TeamItem {
+  userIcon: string;
+  userIconAlt: string;
+  name: string;
+  position: string;
+  ariaLabel: string;
+  socialIcon: string;
+  description: string;
+}
